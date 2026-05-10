4dba6649f13fe742bc92646e779f28cfb06a6278
Apocscode <Apocscode@users.noreply.github.com>
2026-05-10 08:40:39 -0400

    Fix signal direction inversion when applying flag overrides

--- src/main/java/com/apocscode/logiclink/block/SignalTabletItem.java
+++ src/main/java/com/apocscode/logiclink/block/SignalTabletItem.java
@@ -495,7 +495,9 @@ public class SignalTabletItem extends Item {
                 BlockPos relativeTrack = candidate.trackPos().subtract(placementPos);
                 CompoundTag beNbt = new CompoundTag();
                 beNbt.put("TargetTrack", NbtUtils.writeBlockPos(relativeTrack));
-                beNbt.putBoolean("TargetDirection", candidate.front());
+                // Create's TargetDirection is OPPOSITE of our forward semantic,
+                // so invert it when passing to Create
+                beNbt.putBoolean("TargetDirection", !candidate.front());
                 sbe.loadWithComponents(beNbt, level.registryAccess());
 
                 // Register the signal with the track graph immediately (tick() will retry if null)
